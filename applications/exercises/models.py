ea5b67eefcf28349637d7a20bfd3ae1515a7e116
Ivan Karl <ivan6258@gmail.com>
2017-06-16 17:20:35 -0300

    adds exercises model

--- applications/exercises/models.py
+++ applications/exercises/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# vim: set expandtab tabstop=4 shiftwidth=4:
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+
+    video = models.FileField(
+        upload_to='exercises/videos/',
+        storage=settings.DEFAULT_STORAGE
+    )
+
+    notes = models.TextField()
+
--- workoutholic/settings.py
+++ workoutholic/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.core.files.storage import FileSystemStorage
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+UPLOADS_DIR = os.path.join(BASE_DIR, 'uploads'),
+
+DEFAULT_STORAGE = FileSystemStorage(location=UPLOADS_DIR)
