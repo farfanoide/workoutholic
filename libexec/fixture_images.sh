d1be6deaf756db4bff5ab154bb0abf2c26544108
Ivan Karl <ivan6258@gmail.com>
2017-06-19 18:08:55 -0300

    fixes fixtures script

--- libexec/fixture_images.sh
+++ libexec/fixture_images.sh
@@ -3,7 +3,8 @@
 archive_name='fixture_images.tar.gz'
 images_url="https://www.dropbox.com/s/8abt9s859pq9gz2/${archive_name}?dl=1"
 
-curl -LSC - "${images_url}" \
-    -o "${archive_name}" \
-    && tar -xzf "${archive_name}"
+if [ ! -e "${archive_name}" ]; then
+    curl -LSC - "${images_url}" -o "${archive_name}"
+fi
 
+tar -xzf "${archive_name}"
